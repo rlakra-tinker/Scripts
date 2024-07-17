973dfa618ff1be7ff4d5be5b5424ce5ba8478f49
Rohtash <rohtash_lakra@tinkerdiy.com>
2024-07-17 10:43:47 -0700

    merge del files logic

--- OS/OSX/delFilesRecursively.sh
+++ OS/OSX/delFilesRecursively.sh
@@ -1,20 +1,77 @@
 #!/bin/bash
 # Author: Rohtash Lakra
-# Removes the specified file(s) recursively.
-#
+# Removes the spam/unwanted/specified file(s) recursively.
+# Usage:
+#   ./delFilesRecursively 'fileName'
+#   OR
+#   ./delFilesRecursively 'fileName'
 clear
 echo
-#Validate the parameter provided or not.
-if [ -n "$1" ]; then
+HOME_DIR=$PWD
+echo
+
+# Usage
+usage() {
+   echo
+   echo "Usage: ./delFilesRecursively"
+   echo "OR"
+   echo "Usage: ./delFilesRecursively 'FileName'"
+   echo
+}
+
+# files/folders to be removed
+fileExtensions=".DS_Store __pycache__"
+# check which files/folders to remove
+if [[ -z "$1" ]]; then  # No Arguments Supplied
+    # fileExtensions=".DS_Store __pycache__"
+    for entry in $fileExtensions; do
+        filePath="${HOME_DIR}/${entry}"
+        # Folders
+        echo "Analyzing '${entry}' under [${HOME_DIR}] ..."
+        echo    
+        if [[ "${entry:0:1}" == "." ]]; then
+            # Files
+            echo "Removing '${entry}' file(s) recursively ..."
+            echo
+            # find . -type f -name "${entry}" -print -exec echo {} \;
+            find . -type f -name "${entry}" -print -exec rm -rf {} \;
+            echo
+        elif [[ "${entry:0:2}" == "__" ]]; then
+            echo "Removing '${entry}' folders recursively ..."
+            echo
+            # Define the input file
+            TEMP_FILE_PATH="${HOME_DIR}/fileTypes"
+            find "${HOME_DIR}" -type d -name "${entry}" > $TEMP_FILE_PATH
+            # ls -la "${TEMP_FILE_PATH}"
+            IFS=$'\n' # set the Internal Field Separator to newline
+            # read file contents
+            while read -r filePath; do
+                # printf '%s\n' "$filePath"
+                # check the path contains '/venv'
+                if [[ $filePath != *"${HOME_DIR}/venv"* ]]; then
+                #     # ignore 'venv' folder
+                #     echo "Ignoring [${filePath}] ..."
+                # else
+                    # echo "[${filePath}] ..."
+                    rm -rf "${filePath}"
+                fi
+            done < "$TEMP_FILE_PATH"
+            # remove temp file
+            rm "${TEMP_FILE_PATH}"
+        # else
+        #     # Folders
+        #     echo "Removing '${filePath}' folder ..."
+        #     echo
+        #     find . -type d -name "${entry}" -print -exec echo {} \;
+        #     # find . -type d -name "$1" -print -exec rm -rf {} \;
+        #     # find . -type d -depth 4 -name "${entry}" -print;    
+        #     echo
+        fi
+    done
+elif [ -n "$1" ]; then
     echo "Removing '$1' file(s) recursively ..."
     echo
     #find . -type f -name "$1" -print -exec echo {} \;
     find . -type f -name "$1" -print -exec rm -rf {} \;    
-    echo
-else
-   echo "Please, provide file name to be deleted recursively."    
-   echo
-   echo "Usage: ./delFilesRecursively.sh 'FileName'"
-   echo
 fi
-
+echo
