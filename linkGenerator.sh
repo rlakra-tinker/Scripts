bd31cf694bad2dcb3a205b0d0387eb7923e26c2f
Rohtash <rohtash_lakra@tinkerdiy.com>
2024-10-15 11:30:44 -0700

    updated link-generator

--- linkGenerator.sh
+++ linkGenerator.sh
@@ -16,34 +16,39 @@ export MY_OS_CONFIGS="${SCRIPTS}/MyOSConfigs"
 export OSX_DIR="${SCRIPTS}/OS/OSX"
 export GIT_DIR="${SCRIPTS}/VCS/Git"
 
-# ----------------< Tweek Paths >----------------
+# ----------------<Tweak Paths>----------------
 echo
 #echo "${WORKSPACE} not found or is symlink to $(readlink -f ${WORKSPACE})."
 
 # Check the Workspaces folder is under documents or not.
-# if [ ! -d "${WORKSPACE}" -a ! -h "${WORKSPACE}" ]; then 
-#     SCRIPTS="${SCRIPTS}"
-# fi
+if [ ! -d "${WORKSPACE}" -a ! -h "${WORKSPACE}" ]; then
+   WORKSPACE="${CUR_DIR}/Workspace"
+fi
+echo "WORKSPACE: ${WORKSPACE}"
+# Check the Workspaces folder is under documents or not.
+if [ ! -d "${SCRIPTS}" -a ! -h "${SCRIPTS}" ]; then
+   SCRIPTS="${CUR_DIR}"
+fi
 echo "SCRIPTS: ${SCRIPTS}"
 echo
 
-# ----------------< Generate Symbolic Links >----------------
+# ----------------<Generate Symbolic Links>----------------
 
-# ----------------< OSX Links >----------------
+# ----------------<OSX Links>----------------
 ln -sf "${OSX_DIR}/checkPortUsage.sh" ~/checkPortUsage
 ln -sf "${OSX_DIR}/delFilesRecursively.sh" ~/delFilesRecursively
 ln -sf "${OSX_DIR}/showComputerName.sh" ~/showComputerName
 # ln -sf "${OSX_DIR}/delRecursively.sh" ~/delRecursively
 
-# ----------------< AWS Links >----------------
+# ----------------<AWS Links>----------------
 ln -sf "${AWS_DIR}/sshInstance.sh" ~/sshInstance
 
-# ----------------< Docker Links >----------------
+# ----------------<Docker Links>----------------
 ln -sf "${DOCKER_DIR}/buildDockerImage.sh" ~/buildDockerImage
 ln -sf "${DOCKER_DIR}/listDockerImages.sh" ~/listDockerImages
 ln -sf "${DOCKER_DIR}/sshDockerImage.sh" ~/sshDockerImage
 
-# ----------------< Git Links >----------------
+# ----------------<Git Links>----------------
 ln -sf "${GIT_DIR}/addSSHKeys.sh" ~/addSSHKeys
 ln -sf "${GIT_DIR}/logGitCommits.sh" ~/logGitCommits
 ln -sf "${GIT_DIR}/removeBranch.sh" ~/removeGitBranch
@@ -51,9 +56,9 @@ ln -sf "${GIT_DIR}/syncBranches.sh" ~/syncBranches
 ln -sf "${GIT_DIR}/syncBranchWithRemote.sh" ~/syncBranchWithRemote
 ln -sf "${GIT_DIR}/tagBranch.sh" ~/tagGitBranch
 
-# ----------------< Variables >----------------
+# ----------------<Variables>----------------
 
-# ----------------< syncroSVNClient Links >----------------
+# ----------------<syncroSVNClient Links>----------------
 # Generate syncroSVNClient Link
 # brew install openjdk@11
 # sudo ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
@@ -65,3 +70,5 @@ ln -sf "${SYNCRO_SVN_CLIENT}/diffDirsMac.sh" ~/diffFolders
 cd $CUR_DIR
 echo
 
+--------
+lrwxr-xr-x   1 tinker  staff      65 Jul  9 12:10 delSpamFilesRecursively -> /Users/tinker/Workspace/Scripts/OS/OSX/delSpamFilesRecursively.sh
