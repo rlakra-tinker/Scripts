6dde5ecbe5537193c0498e4da76bea64cbdc8b53
Rohtash <rohtash_lakra@tinkerdiy.com>
2024-10-15 11:57:20 -0700

    update link-generator

--- linkGenerator.sh
+++ linkGenerator.sh
@@ -9,14 +9,8 @@ export USER_HOME="${HOME}"
 export APPS_DIR="/Applications"
 export WORKSPACE="${USER_HOME}/Workspace"
 export SCRIPTS="${WORKSPACE}/Scripts"
-export BUILD_TOOLS="${SCRIPTS}/BuildTools"
-export AWS_DIR="${SCRIPTS}/AWS"
-export DOCKER_DIR="${SCRIPTS}/Docker"
-export MY_OS_CONFIGS="${SCRIPTS}/MyOSConfigs"
-export OSX_DIR="${SCRIPTS}/OS/OSX"
-export GIT_DIR="${SCRIPTS}/VCS/Git"
 
-# ----------------<Tweak Paths>----------------
+# --------<Tweak Paths, if needed>--------
 echo
 #echo "${WORKSPACE} not found or is symlink to $(readlink -f ${WORKSPACE})."
 
@@ -34,37 +28,49 @@ echo
 
 # ----------------<Generate Symbolic Links>----------------
 
-# ----------------<OSX Links>----------------
-ln -sf "${OSX_DIR}/checkPortUsage.sh" ~/checkPortUsage
-ln -sf "${OSX_DIR}/delFilesRecursively.sh" ~/delFilesRecursively
-ln -sf "${OSX_DIR}/showComputerName.sh" ~/showComputerName
-# ln -sf "${OSX_DIR}/delRecursively.sh" ~/delRecursively
-
 # ----------------<AWS Links>----------------
-ln -sf "${AWS_DIR}/sshInstance.sh" ~/sshInstance
+export AWS_DIR="${SCRIPTS}/AWS"
+ln -fs "${AWS_DIR}/sshInstance.sh" ~/sshInstance
+
+# ----------------<BuildTools Links>----------------
+export BUILD_TOOLS="${SCRIPTS}/BuildTools"
+ln -fs "${BUILD_TOOLS}/Homebrew/installHomebrew.sh" ~/installHomebrew
 
 # ----------------<Docker Links>----------------
-ln -sf "${DOCKER_DIR}/buildDockerImage.sh" ~/buildDockerImage
-ln -sf "${DOCKER_DIR}/listDockerImages.sh" ~/listDockerImages
-ln -sf "${DOCKER_DIR}/sshDockerImage.sh" ~/sshDockerImage
+export DOCKER_DIR="${SCRIPTS}/Docker"
+ln -fs "${DOCKER_DIR}/buildDockerImage.sh" ~/buildDockerImage
+ln -fs "${DOCKER_DIR}/listDockerImages.sh" ~/listDockerImages
+ln -fs "${DOCKER_DIR}/sshDockerImage.sh" ~/sshDockerImage
 
 # ----------------<Git Links>----------------
-ln -sf "${GIT_DIR}/addSSHKeys.sh" ~/addSSHKeys
-ln -sf "${GIT_DIR}/logGitCommits.sh" ~/logGitCommits
-ln -sf "${GIT_DIR}/removeBranch.sh" ~/removeGitBranch
-ln -sf "${GIT_DIR}/syncBranches.sh" ~/syncBranches
-ln -sf "${GIT_DIR}/syncBranchWithRemote.sh" ~/syncBranchWithRemote
-ln -sf "${GIT_DIR}/tagBranch.sh" ~/tagGitBranch
+export GIT_DIR="${SCRIPTS}/VCS/Git"
+ln -fs "${GIT_DIR}/addSSHKeys.sh" ~/addSSHKeys
+ln -fs "${GIT_DIR}/logGitCommits.sh" ~/logGitCommits
+ln -fs "${GIT_DIR}/removeBranch.sh" ~/removeGitBranch
+ln -fs "${GIT_DIR}/syncBranches.sh" ~/syncBranches
+ln -fs "${GIT_DIR}/syncBranchWithRemote.sh" ~/syncBranchWithRemote
+ln -fs "${GIT_DIR}/tagBranch.sh" ~/tagGitBranch
+
+# ----------------<MyOSConfigs Links>----------------
+export MY_OS_CONFIGS="${SCRIPTS}/MyOSConfigs"
+ln -fs "${MY_OS_CONFIGS}/backUpLocalConfigs.sh" ~/backUpLocalConfigs
+
+# ----------------<OSX Links>----------------
+export OSX_DIR="${SCRIPTS}/OS/OSX"
+ln -fs "${OSX_DIR}/checkPortUsage.sh" ~/checkPortUsage
+ln -fs "${OSX_DIR}/delFilesRecursively.sh" ~/delFilesRecursively
+ln -fs "${OSX_DIR}/showComputerName.sh" ~/showComputerName
+# ln -fs "${OSX_DIR}/delRecursively.sh" ~/delRecursively
 
 # ----------------<Variables>----------------
 
 # ----------------<syncroSVNClient Links>----------------
 # Generate syncroSVNClient Link
 # brew install openjdk@11
-# sudo ln -sfn /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+# sudo ln -fs /opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
 #
 SYNCRO_SVN_CLIENT="${APPS_DIR}/syncroSVNClient"
-ln -sf "${SYNCRO_SVN_CLIENT}/diffDirsMac.sh" ~/diffFolders
+ln -fs "${SYNCRO_SVN_CLIENT}/diffDirsMac.sh" ~/diffFolders
 
 # move back to current dir
 cd $CUR_DIR
